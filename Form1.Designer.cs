32346dfe29f9583703eee0b5936719c6a2087af1
Alex_Sour <52638772+Alex-Sour@users.noreply.github.com>
2021-05-09 17:06:10 -0300

    added  e v e r y t h i n g

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CS_Compiler_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 366);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please drag-and-drop a .cs file to start.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(14, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(530, 243);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Awaiting file input";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(19, 79);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 271);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(18, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(562, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rebuild";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(19, 15);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Open on build";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "C# Compiler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace CS_Compiler_Interface
+{
+    public partial class Form1 : Form
+    {
+        static string output = "";
+        static string prev = "";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 2)
+            {
+                prev = args[1];
+                textBox1.Text = "Processing...";
+                output = "";
+                var startInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    WorkingDirectory = Path.GetDirectoryName(args[1]),
+                    FileName = "powershell.exe",
+                    Arguments = "\"C:\\Windows\\Microsoft.NET\\Framework64\\v3.5\\csc.exe\" \"\\\"" + args[1] + "\\\"\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+                System.Diagnostics.Process a = System.Diagnostics.Process.Start(startInfo);
+                a.OutputDataReceived += new DataReceivedEventHandler((object thesender, DataReceivedEventArgs ee) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(ee.Data))
+                    {
+                        output += ee.Data + Environment.NewLine;
+                        // output.Append("\n[" + lineCount + "]: " + e.Data);
+                    }
+                });
+                a.BeginOutputReadLine();
+                a.WaitForExit();
+                textBox1.Text = output;
+                MessageBox.Show("Finished building 1 file.", "Finished building", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (!checkBox1.Checked)
+                {
+                    Process.Start(Path.GetDirectoryName(args[1]));
+                }
+                else
+                {
+                    Process.Start(Path.Combine(Path.GetDirectoryName(args[1]), Path.GetFileNameWithoutExtension(args[1]) + ".exe"));
+                }
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void textBox1_Enter(object sender, EventArgs e)
+        {
+            ActiveControl = label1;
+        }
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                prev = file;
+                textBox1.Text = "Processing...";
+                output = "";
+                var startInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    WorkingDirectory = Path.GetDirectoryName(file),
+                    FileName = "powershell.exe",
+                    Arguments = "\"C:\\Windows\\Microsoft.NET\\Framework64\\v3.5\\csc.exe\" \"\\\"" + file + "\\\"\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+            };
+                System.Diagnostics.Process a = System.Diagnostics.Process.Start(startInfo);
+                a.OutputDataReceived += new DataReceivedEventHandler((object thesender, DataReceivedEventArgs ee) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(ee.Data))
+                    {
+                        output += ee.Data + Environment.NewLine;
+                       // output.Append("\n[" + lineCount + "]: " + e.Data);
+                    }
+                });
+                a.BeginOutputReadLine();
+                a.WaitForExit();
+                textBox1.Text = output;
+                MessageBox.Show("Finished building 1 file.", "Finished building", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (!checkBox1.Checked)
+                {
+                    Process.Start(Path.GetDirectoryName(file));
+                }
+                else
+                {
+                    Process.Start(Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + ".exe"));
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(prev != "")
+            {
+                textBox1.Text = "Processing...";
+                output = "";
+                var startInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    WorkingDirectory = Path.GetDirectoryName(prev),
+                    FileName = "powershell.exe",
+                    Arguments = "\"C:\\Windows\\Microsoft.NET\\Framework64\\v3.5\\csc.exe\" \"\\\"" + prev + "\\\"\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+                System.Diagnostics.Process a = System.Diagnostics.Process.Start(startInfo);
+                a.OutputDataReceived += new DataReceivedEventHandler((object thesender, DataReceivedEventArgs ee) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(ee.Data))
+                    {
+                        output += ee.Data + Environment.NewLine;
+                        // output.Append("\n[" + lineCount + "]: " + e.Data);
+                    }
+                });
+                a.BeginOutputReadLine();
+                a.WaitForExit();
+                textBox1.Text = output;
+                MessageBox.Show("Finished building 1 file.", "Finished building", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if(!checkBox1.Checked)
+                {
+                    Process.Start(Path.GetDirectoryName(prev));
+                }
+                else
+                {
+                    Process.Start(Path.Combine(Path.GetDirectoryName(prev), Path.GetFileNameWithoutExtension(prev) + ".exe"));
+                }
+            }
+            
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
